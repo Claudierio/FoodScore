21b24f1b4800d8dbaaa29f234f7e07f0fb0efa17
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-13 14:31:12 -0300

    feat: commit inicial.

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('FoodScore API')
+    .setDescription(
+      'FoodScore é uma api que busca propor ao usuário a opção de avaliar comidas e restaurantes, além de poder visualizar as avaliações de outros usuários.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
