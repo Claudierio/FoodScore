72c9dbc069bf30d0ade410b298847b8c4f31e057
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-13 14:59:47 -0300

    feat: CRUD para usuarios.

---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './repositories/users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.repository.create(createUserDto);
+  }
+
+  async findAll() {
+    return await this.repository.findAll();
+  }
+
+  async findOne(id: string) {
+    return await this.repository.findOne(id);
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return this.repository.update(id, updateUserDto);
+  }
+
+  async remove(id: string) {
+    return this.repository.remove(id);
+  }
+}
