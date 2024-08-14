11156f769bd7539474058e123821f73dfad52361
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-14 15:13:14 -0300

    feat: adicao de rotas protegidas pelo token para restaurant e review.

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -6,11 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/common/guards/auth.guard';
 
 @ApiTags('restaurants')
 @Controller('restaurants')
@@ -18,21 +20,25 @@ export class RestaurantsController {
   constructor(private readonly restaurantsService: RestaurantsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createRestaurantDto: CreateRestaurantDto) {
     return this.restaurantsService.create(createRestaurantDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.restaurantsService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.restaurantsService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(
     @Param('id') id: string,
     @Body() updateRestaurantDto: UpdateRestaurantDto,
@@ -41,6 +47,7 @@ export class RestaurantsController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.restaurantsService.remove(id);
   }
