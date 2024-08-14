47accfa9a1ac8325aac5d238878501a748d8b2e3
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-08-14 14:28:53 -0300

    feat: adicao da validacao para que a descriacao tenha ate 500 caracteres.

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -3,28 +3,36 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 import { ReviewsRepository } from './repositories/reviews.repository';
 import { ReviewEntity } from './entities/review.entity';
+import { BadRequestError } from 'src/common/errors/types/BadRequestError';
 
 @Injectable()
 export class ReviewsService {
   constructor(private readonly repository: ReviewsRepository) {}
 
-  create(createReviewDto: CreateReviewDto): Promise<ReviewEntity> {
+  async create(createReviewDto: CreateReviewDto): Promise<ReviewEntity> {
+    const { description } = createReviewDto;
+    if (description.length > 500) {
+      throw new BadRequestError('Descrição deve conter até 500 caracteres.');
+    }
     return this.repository.create(createReviewDto);
   }
 
-  async findAll() {
-    return await this.repository.findAll();
+  async findAll(): Promise<ReviewEntity[]> {
+    return this.repository.findAll();
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<ReviewEntity> {
     return this.repository.findOne(id);
   }
 
-  update(id: string, updateReviewDto: UpdateReviewDto) {
+  async update(
+    id: string,
+    updateReviewDto: UpdateReviewDto,
+  ): Promise<ReviewEntity> {
     return this.repository.update(id, updateReviewDto);
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<ReviewEntity> {
     return await this.repository.remove(id);
   }
 }
